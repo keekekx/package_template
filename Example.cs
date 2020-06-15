7af13b5e6600ce2b45319547c8314e848a8cf903
kexiang <kexiang@thinkplay.co>
2020-06-15 17:29:11 +0800

    add example

---
+++ Example.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Example
+{
+    [MenuItem("PackageTemplate/Test")]
+    public static void Test()
+    {
+        Debug.Log("Hello World");
+    }
+}
